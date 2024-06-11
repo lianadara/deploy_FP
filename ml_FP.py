aec0b436227a93aebd97862d96ef09147f98e761
Berliana Fitria Dewi <106791473+lianadara@users.noreply.github.com>
2024-06-11 19:16:43 +0700

    Update ml_FP.py

--- ml_FP.py
+++ ml_FP.py
@@ -86,11 +86,11 @@ def run():
         )
 
         answer = query_engine.chat(prompt)
-        # if answer is None:
-        #     st.write("Sorry, I can't answer the question yet")
-        # else:
-        st.write("### Response:")
-        st.write(answer)
+        if answer:
+            st.write("### Response:")
+            st.write(answer)
+        else:
+            st.write("Sorry, I can't answer the question yet")
 
 if __name__ == '__main__':
     run()
