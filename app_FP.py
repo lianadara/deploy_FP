f5cdc99b732eadc6cbc93d250876f1941105f484
Berliana Fitria Dewi <106791473+lianadara@users.noreply.github.com>
2024-06-11 18:06:34 +0700

    Update app_FP.py

--- app_FP.py
+++ app_FP.py
@@ -4,7 +4,7 @@ import predict as predict
 import ml_FP as ml
 
 navigation = st.sidebar.selectbox('🔗Select Page:', ('📃Form Page','📂File Upload Page','💬Chatbot Page'))
-st.sidebar.header("Company name")
+st.sidebar.header("by Team 2")
 
 if navigation == '📃Form Page':
     predict_FP.run()
